bf2c7796cd2b44bcc11f66fd15ee39e37d01aae6
Ifycode <marychord@gmail.com>
2025-01-05 03:11:28 +0100

    Add expenses list, total and validation alert

--- 3-ionic-component-basics/app.js
+++ 3-ionic-component-basics/app.js
@@ -2,14 +2,43 @@ const inputReason = document.querySelector('#input-reason');
 const inputAmount = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
 
-confirmBtn.addEventListener('click', () => {
+let totalExpenses = 0;
+
+const clear = () => {
+    inputReason.value = '';
+    inputAmount.value = '';
+};
+
+const presentAlert = async () => {
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Invalid Input';
+    alert.message = 'Please enter a valid reason and amount!';
+    alert.buttons = ['Okay'];
+
+    document.body.appendChild(alert);
+    await alert.present();
+}
+
+confirmBtn.addEventListener('click', async () => {
     const enteredReason = inputReason.value;
     const enteredAmount = inputAmount.value;
 
     if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
+        presentAlert();
         return;
     }
 
-    console.log(enteredReason, enteredAmount);
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+    expensesList.appendChild(newItem);
+
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+
+    clear();
 });
+
+cancelBtn.addEventListener('click', clear);
